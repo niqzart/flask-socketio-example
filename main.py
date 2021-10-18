22af1442bdb4e99cd290fb758598aec8ef054fd0
qzart45hi <qwert45hi@yandex.ru>
2021-10-18 12:15:54 +0300

    Update istuff

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ def index():
     return render_template("index.html", )
 
 
-@socketio.on("send_message")
-def handle_source(json_data):
-    text = json_data["message"].encode("ascii", "ignore")
-    socketio.emit("echo", {"echo": "Server Says: " + text})
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
 
 
 if __name__ == "__main__":
